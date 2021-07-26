1ae743a9894f8eb2e800ec4a53990ba3dcc68ea3
saiTama-max <vinamraj01@gmail.com>
2021-07-26 11:02:00 +0530

    change food spawn rate and record population in arrays

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,11 @@ fn window_conf() -> Conf {
 async fn main() {
     let mut all_objects: Vec<Entity> = Vec::new();
     let mut spawn_food = 0.0;
+    let mut add_to_pop = 0.0;
+    let mut population: Vec<u32> = Vec::new();
+    let mut food: Vec<u32> = Vec::new();
     srand(macroquad::miniquad::date::now() as u64);
-    for _ in 0..15 {
+    for _ in 0..3 {
         let angle = gen_range(0.0, std::f32::consts::PI * 2.);
         let pos = vec2(gen_range(10.0, 1250.0), gen_range(10.0, 710.0));
         all_objects.push(Organism {
@@ -31,20 +34,38 @@ async fn main() {
             velocity: Vec2::ZERO,
         });
     }
+    population.push(3);
 
-    for _ in 0..20 {
+    for _ in 0..50 {
         all_objects.push(Food {
             energy: gen_range(0.3, 1.0),
             rad: 3.0,
             pos: vec2(gen_range(3.0, 1257.0), gen_range(3.0, 717.0)),
         });
     }
+    food.push(50);
 
     let mut qtree = QuadTree::new(0, [0.0, 0.0, 1260.0, 720.0]);
     loop {
+        if is_key_pressed(KeyCode::Escape) {
+            break;
+        }
         clear_background(BLACK);
         qtree.clear();
         spawn_food += get_frame_time();
+        add_to_pop += get_frame_time();
+
+        if spawn_food >= 3.0 {
+            spawn_food = 0.0;
+            for _ in 0..25 {
+                all_objects.push(Food {
+                    energy: gen_range(0.3, 1.0),
+                    rad: 3.0,
+                    pos: vec2(gen_range(3.0, 1257.0), gen_range(3.0, 717.0)),
+                });
+            }
+        } 
+
         for org in all_objects.iter_mut() {
             match org {
                 Organism { .. } => org.update(),
@@ -55,22 +76,12 @@ async fn main() {
             }
         }
 
-        if spawn_food >= 5.0 {
-            spawn_food = 0.0;
-            for _ in 0..5 {
-                all_objects.push(Food {
-                    energy: gen_range(0.3, 1.0),
-                    rad: 3.0,
-                    pos: vec2(gen_range(3.0, 1257.0), gen_range(3.0, 717.0)),
-                });
-            }
-        }
-
         let mut return_objects: Vec<Entity>;
         let mut food_to_remove: Vec<Vec2> = Vec::new();
         let mut organisms_to_remove: Vec<Vec2> = Vec::new();
         let mut organisms_to_add: Vec<Vec2> = Vec::new();
         let mut organims_count = 0;
+        let mut food_count = 0;
         for obj in all_objects.iter_mut() {
             if let Organism {
                 pos: objpos,
@@ -118,8 +129,17 @@ async fn main() {
                         organisms_to_add.push(*op);
                     }
                 }
+            } else {
+                food_count += 1;
             }
         }
+
+        if add_to_pop >= 1.0 {
+             population.push(organims_count);
+             food.push(food_count);
+             add_to_pop = 0.0;
+        }
+
         if !food_to_remove.is_empty() {
             all_objects.retain(|o| match o {
                 Food { pos, .. } => !food_to_remove.contains(pos),
@@ -152,4 +172,6 @@ async fn main() {
         );
         next_frame().await
     }
+    println!("Population: {:?}", population);
+    println!("Food: {:?}", food);
 }
